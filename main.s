3eebbacd619561af7970dd10f7280b0404b7fa22
pheo <pheo@pheo-desktop.att.net>
2016-08-11 22:42:12 -0700

    -fno-asynchronous-unwind-tables (why not)

--- main.s
+++ main.s
@@ -3,19 +3,11 @@
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
-	.cfi_startproc
 	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
 	nop
 	popq	%rbp
-	.cfi_def_cfa 7, 8
 	ret
-	.cfi_endproc
-.LFE0:
 	.size	main, .-main
 	.ident	"GCC: (GNU) 5.3.1 20160406 (Red Hat 5.3.1-6)"
 	.section	.note.GNU-stack,"",@progbits
