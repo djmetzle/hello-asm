ec31a5dbfba0b393ada242ab17d5c7c7f3035d5b
pheo <pheo@pheo-desktop.att.net>
2016-08-12 09:23:43 -0700

    Add Fizz and Buzz

--- main.s
+++ main.s
@@ -6,13 +6,15 @@ main:
 	pushq	%rbp
 	movq	%rsp, %rbp
 
-	call func
+	call print_hello
+	call print_fizz
+	call print_buzz
 
 	popq	%rbp
 	ret
 	.size	main, .-main
 
-func:
+print_hello:
         pushq   %rbp
         movq    %rsp, %rbp
 
@@ -20,19 +22,60 @@ func:
 	movq	$1, %rax
 	movq	$1, %rdi
 	movq	$hello_msg, %rsi
-	movq	$len, %rdx
+	movq	$hello_len, %rdx
 	syscall
 
         popq    %rbp
         ret
-        .size   func, .-func
+        .size   print_hello, .-print_hello
         .globl  main
         .type   main, @function
 
+print_fizz:
+        pushq   %rbp
+        movq    %rsp, %rbp
+
+	# hello print	
+	movq	$1, %rax
+	movq	$1, %rdi
+	movq	$fizz_msg, %rsi
+	movq	$fizz_len, %rdx
+	syscall
+
+        popq    %rbp
+        ret
+        .size   print_fizz, . - print_fizz
+        .globl  main
+        .type   main, @function
+
+print_buzz:
+        pushq   %rbp
+        movq    %rsp, %rbp
+
+	# hello print	
+	movq	$1, %rax
+	movq	$1, %rdi
+	movq	$buzz_msg, %rsi
+	movq	$buzz_len, %rdx
+	syscall
+
+        popq    %rbp
+        ret
+        .size   print_buzz, . - print_buzz
+        .globl  main
+        .type   main, @function
 
 	.data
 
 hello_msg:
 	.ascii	"Hello Assembler\n"	
-	len = . - hello_msg
+	hello_len = . - hello_msg
+
+fizz_msg:
+	.ascii	"Fizz\n"	
+	fizz_len = . - fizz_msg
+
+buzz_msg:
+	.ascii	"Buzz\n"	
+	buzz_len = . - buzz_msg
 
