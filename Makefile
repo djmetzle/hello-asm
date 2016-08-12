c670e2c208ce3eeb6cbdfa9133d1716078b4610f
pheo <pheo@pheo-desktop.att.net>
2016-08-12 09:10:45 -0700

    Add Hello World (and a Makefile)

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	gcc main.s -o main
+clean:
+	rm -f main
--- main.s
+++ main.s
@@ -5,9 +5,21 @@
 main:
 	pushq	%rbp
 	movq	%rsp, %rbp
-	nop
+
+	# hello print	
+	movq	$1, %rax
+	movq	$1, %rdi
+	movq	$hello_msg, %rsi
+	movq	$len, %rdx
+	syscall
+
 	popq	%rbp
 	ret
 	.size	main, .-main
 	.ident	"GCC: (GNU) 5.3.1 20160406 (Red Hat 5.3.1-6)"
 	.section	.note.GNU-stack,"",@progbits
+
+	.data
+hello_msg:
+	.ascii	"Hello Assembler\n"	
+	len = . - hello_msg
